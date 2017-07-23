84f5f0ca9f5ff8bced35e14b811438a26d15751d
xb <3084662911@qq.com>
2017-07-23 14:34:44 +0800

    set the arguments -DANDROID_STL to c++_shared

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,72 @@
+//
+// Created by xiongda on 2017/7/22.
+//
+
 #include <jni.h>
 #include <string>
+#include <vector>
+#include <iostream>
+
+#include "dlib/image_io.h"
+#include "dlib/image_processing.h"
+#include "dlib/image_processing/frontal_face_detector.h"
+
+#include <ctime>
+
+dlib::frontal_face_detector m_facedetector = dlib::get_frontal_face_detector();
+
+//获取人脸框
+std::vector<int> getfacerect(const std::vector<int> img, int height, int width) {
+    dlib::array2d<unsigned char> image;
+    image.set_size(height, width);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int clr = img[i * width + j];
+            int red = (clr & 0x00ff0000) >> 16;//取高两位
+            int green = (clr & 0x0000ff00) >> 8;//取中两位
+            int blue = clr & 0x000000ff;//取低两位
+
+            unsigned char gray = red * 0.299 + green * 0.587 + blue * 0.114;
+
+            //dlib::rgb_pixel pt(red,green.blue);
+            image[i][j] = gray;
+
+        }
+    }
+
+    clock_t begin = clock();
+    std::vector<dlib::rectangle> dets = m_facedetector(image);
+
+    std::vector<int> rect;
+
+    if (!dets.empty()) {
+        rect.push_back(dets[0].left());
+        rect.push_back(dets[0].right());
+        rect.push_back(dets[0].width());
+        rect.push_back(dets[0].height());
+    }
+    return rect;
+}
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_xiongda_dlib4android_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+JNIEXPORT jintArray JNICALL
+Java_com_xiongda_dlib4android_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */, jintArray image_data,
+                                                         jint image_height, jint image_width) {
+
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+    std::vector<int> image_datacpp(image_height * image_width, image_width);
+    jsize len = env->GetArrayLength(image_data);
+
+    jint *body = env->GetIntArrayElements(image_data, 0);
+
+    for (jsize i = 0; i < len; i++) {
+        image_datacpp[i] = (int) body[i];
+    }
+    std::vector<int > rect=getfacerect(image_datacpp,image_height,image_width);
+    jintArray result=env->NewIntArray(4);
+    env->SetIntArrayRegion(result,0,4,&rect[0]);
+
+    return result;
 }
--- app/src/main/java/com/xiongda/dlib4android/MainActivity.java
+++ app/src/main/java/com/xiongda/dlib4android/MainActivity.java
@@ -1,15 +1,33 @@
 package com.xiongda.dlib4android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("dlib");
     }
+
+    public static final int REQUESTCODE_PICK = 0X002;
+    private Button btnOpenImage;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +35,94 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mImageView = (ImageView) findViewById(R.id.iv_image);
+        //        tv.setText(stringFromJNI());
+    }
+
+    public void onClickOpenImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, REQUESTCODE_PICK);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native int[] stringFromJNI(int[] pixels, int height, int width);
+
+    private int[] face_detection(Bitmap origin_image) {
+        float scale = 240.f / Math.max(origin_image.getHeight(), origin_image.getWidth());
+        int width = (int) (origin_image.getWidth() * scale);
+        int height = (int) (origin_image.getHeight() * scale);
+        Bitmap resize_image = Bitmap.createScaledBitmap(origin_image, width, height, false);
+
+        // 保存所有像素的数组,图片宽x高
+        int[] pixels = new int[width * height];
+
+        resize_image.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        int[] rect = stringFromJNI(pixels, height, width);
+        int[] result_rect = new int[4];
+        result_rect[0] = (int) (rect[0] / scale);
+        result_rect[1] = (int) (rect[1] / scale);
+        result_rect[2] = (int) (rect[2] / scale);
+        result_rect[3] = (int) (rect[3] / scale);
+        result_rect[2] = result_rect[2] + result_rect[0];
+        result_rect[3] = result_rect[3] + result_rect[1];
+        return result_rect;
+    }
+
+    private void drawResult(Bitmap scaled) {
+        int[] rect = face_detection(scaled);
+        Canvas canvas = new Canvas(scaled.copy(Bitmap.Config.ARGB_8888,true));
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3.0f);
+        canvas.drawLine(rect[0], rect[1], rect[2], rect[1], paint);//up
+        canvas.drawLine(rect[0], rect[1], rect[0], rect[3], paint);//left
+        canvas.drawLine(rect[0], rect[3], rect[2], rect[3], paint);//down
+        canvas.drawLine(rect[2], rect[1], rect[2], rect[3], paint);//right
+        mImageView.setImageBitmap(scaled);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUESTCODE_PICK) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                drawResult(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static String getRealFilePath(final Context context, final Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver()
+                                   .query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
 }
